fc48388e4264c33c15971ced00b17fd6cef4a646
Bibek Pandey <36302659+bibekhazard@users.noreply.github.com>
2025-02-13 03:33:20 -0500

    Update main.tf

--- ps-template/main.tf
+++ ps-template/main.tf
@@ -9,7 +9,7 @@ locals {
     "read" : var.read_access_level,
     "write" : var.write_access_level,
     "list" : var.list_access_level,
-    "tagging" : var.tagging_access_level
+    "tagging" : var.tagging_access_level,  # ✅ Added missing comma
     "permissions-management" : var.permissions_management_access_level,
     "wildcard-only" : {
       "single-actions" : var.wildcard_only_single_actions,
@@ -17,21 +17,37 @@ locals {
       "service-write" : var.wildcard_only_write_service,
       "service-list" : var.wildcard_only_list_service,
       "service-tagging" : var.wildcard_only_tagging_service,
-      "service-permissions-management" : var.wildcard_only_permissions_management_service,
-    }
+      "service-permissions-management" : var.wildcard_only_permissions_management_service
+    },
+    "exclude-actions" : var.exclude_actions,
+    "skip-resource-constraints" : var.skip_resource_constraints
   }
   rendered_template = jsonencode(local.policy_sentry_template)
   decoded_template  = jsondecode(jsonencode(local.policy_sentry_template))
-  policy_sentry     = [ "policy_sentry", "write-policy", "--fmt", "terraform" ]
-  command           = var.minimize ? concat(local.policy_sentry, [ "--minimize" ]) : local.policy_sentry
+  policy_sentry     = ["policy_sentry", "write-policy", "--fmt", "terraform"]
+  command           = var.minimize ? concat(local.policy_sentry, ["--minimize"]) : local.policy_sentry
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Generates the JSON policy template file
+# ---------------------------------------------------------------------------------------------------------------------
 
 resource "local_file" "template" {
-  filename =  "template.json"
-  content = local.rendered_template
+  filename = "template-${random_string.random_template_name.result}.json"
+  content  = local.rendered_template
 }
+
+resource "random_string" "random_template_name" {
+  length  = 24
+  special = false
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Run external command to process the policy file
+# ---------------------------------------------------------------------------------------------------------------------
 
 data "external" "policy" {
-  program = concat(local.command, [ "--input-file", local_file.template.filename ])
+  program = concat(local.command, ["--input-file", local_file.template.filename])
+  
+  depends_on = [local_file.template]
 }
-
