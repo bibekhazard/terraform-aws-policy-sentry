2630787f0e9c66e02f1acdfb180db9bb72098978
Bibek Pandey <36302659+bibekhazard@users.noreply.github.com>
2025-02-13 01:26:38 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,12 @@
 terraform {}
 
 provider "aws" {
-      source  = "hashicorp/aws"
       version = ">= 4.0.0"
 }
 
 provider "template" {}
 
 provider "local" {
-     source  = "hashicorp/template"
       version = ">= 2.2.0"
 }
 
