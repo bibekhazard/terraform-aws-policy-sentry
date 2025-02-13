2de6ab8665f1c88a2ded3c5824f789c3d046c319
Bibek Pandey <36302659+bibekhazard@users.noreply.github.com>
2025-02-13 01:36:42 -0500

    Update provider.tf

--- iam-policies/provider.tf
+++ iam-policies/provider.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-}
+terraform {}
 
-provider "aws" {
-  version = "~> 2.48.0"
-  region  = var.region
-}
+provider "aws" {}
 
-provider "template" {
-  version = "~> 2.1.2"
-}
+provider "template" {}
